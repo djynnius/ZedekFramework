b2ee626f17a83ccd8850e8cc7b7645a48245bbe8
Ikakke Ikpe <djynnius@gmail.com>
2013-10-21 07:59:43 -0700

    Update anchor.php

--- anchor.php
+++ anchor.php
@@ -4,9 +4,9 @@ namespace __zf__;
 session_start();
 
 #application root /path/to/zedek/ 
-const zroot = "/var/djynnius/projects/zedek2.0.1/";
-const zweb = "/var/www/";
-const zsubpath = "";
+const zroot = "/path/to/zedek/";
+const zweb = "/path/to/web/root/";
+const zsubpath = "path/to/web/sub/folder";
 
 #set include path
 $os = strtolower($_SERVER['SERVER_SOFTWARE']);
