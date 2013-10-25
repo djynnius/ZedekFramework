a54dab3ff729b62ce2104bdec603b9675ac40dd4
Ikakke Ikpe <ikakke@djynn-ubuntu.(none)>
2013-10-25 17:05:10 +0100

    config.json corrected

--- public/router.php
+++ public/router.php
@@ -1,19 +1,4 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-	require_once "../controller.php";
+	#path/to/controller.php
+	require_once "../controller.php"; 
 ?>
-=======
-=======
-<<<<<<< HEAD
-	require_once "../controller.php";
-?>
-=======
-	require_once "/path/to/zedek/anchor.php";
-?>
->>>>>>> b2ee626f17a83ccd8850e8cc7b7645a48245bbe8
-=======
->>>>>>> 1eee4a11512906aae59d27706de5fa1822a07599
-	require_once "/path/to/zedek/anchor.php";
-?>
->>>>>>> b2ee626f17a83ccd8850e8cc7b7645a48245bbe8
