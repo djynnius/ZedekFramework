63f5b8cafadb53cb5e4cc175a8b75f8150a98afd
Ikakke Ikpe <psilent@gmail.com>
2015-06-24 12:02:01 +0100

    version 3.15.35 included internals for email messaging and global email param

---
+++ internals/messenger.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace __zf__;
+
+class _Msg{
+	static public function send($post){
+		$config = new ZConfig;
+		foreach($post as $k=>$item){
+			$$k = $item;
+		}
+
+		$to = self::fixEmailAddresses($to);
+		$cc = self::fixEmailAddresses($cc);
+
+		$header  = 'MIME-Version: 1.0' . "\r\n";
+		$header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";		
+		$header .= "To: {$to}"."\r\n";
+		$header .= "From: ".$config->get('app')." <".$config->get('email').">"."\r\n";
+		$header .= "Cc: {$cc}"."\r\n";
+
+		mail($to, $subject, $message, $header);
+
+	}
+
+	private function fixEmailAddresses($to){
+		$to = trim($to);
+		$to = explode(" ", $to);
+
+		$o = array();
+		foreach($to as $i=>$v){
+			$v = trim($v);
+			$v = explode(",", $v);
+			foreach($v as $j=>$u){
+				$u = trim($u);
+				if(!empty($u)) $o[] = $u;
+			}
+		}
+
+		$to = null;
+		$to = join(",", $o);
+		return $to;		
+	}
+
+}
