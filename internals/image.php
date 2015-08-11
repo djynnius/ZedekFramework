50d96d183e3b0ce84bb190f70550ef20e8d84a7c
Ikakke Ikpe <psilent@gmail.com>
2015-08-11 09:42:43 +0100

    version 4.8.6.11082015 Modified Image helper(internal) to intelligently determine image type and included method ext to return file extension

--- internals/image.php
+++ internals/image.php
@@ -4,12 +4,18 @@ namespace __zf__;
 
 class _Image {
 
-	function extension($file){
+	static public function extension($file){
 		$ext = explode(".", $file);
-		return end($ext);
+		$ext = end($ext);
+		$ext = strtolower($ext);
+		return $ext;
 	}
 
-	function crop($source, $target, $width=256, $height=256, $ext="jpeg"){
+	static public function ext($file){
+		return self::extension($file);
+	}
+
+	static public function crop($source, $target, $width=256, $height=256, $ext="auto"){
 		list($w, $h) = getimagesize($source);
 		$src_w = ($w/2)-($width/2);
 		$src_h = ($h/2)-($height/2);
@@ -38,14 +44,14 @@ class _Image {
 				imagegif($canvas, $target, 100);
 				break;
 			case "png":
-				imagegif($canvas, $target, 100);
+				imagepng($canvas, $target, 100);
 				break;
 			default:
 				imagejpeg($canvas, $target, 100);		
 		}
 	}
 
-	function resize($source, $target, $newWidth = 512, $newHeight = 512, $mime = "jpeg"){
+	static public function resize($source, $target, $newWidth = 512, $newHeight = 512, $mime = "auto"){
 		$mime = strtolower($mime);
 		$sourceStats = getimagesize($source);
 		$sourceRawDimensions = $sourceStats[3];
@@ -79,86 +85,11 @@ class _Image {
 				imagegif($canvas, $target, 100);
 				break;
 			case "png":
-				imagegif($canvas, $target, 100);
+				imagepng($canvas, $target, 100);
 				break;
 			default:
 				imagejpeg($canvas, $target, 100);		
 		}			
 	}
 		
-	function batchResize($sourceFolder, $destinationFolder){
-		self::progressBar($sourceFolder, $destinationFolder);
-		if(is_dir($sourceFolder) and is_dir($destinationFolder)){
-			$handleS = opendir($sourceFolder);
-			while($file = readdir($handleS)){
-				if(!is_dir($file)){
-					$fileStat = @getimagesize($sourceFolder."/".$file);
-					if($fileStat['mime'] == "image/jpeg"){
-						$transition = new Image();
-						$transition->resize($sourceFolder."/".$file, $destinationFolder."/".$file);
-					}
-				}
-			}
-			closedir($handleS);
-		} else {
-			echo "Either your source or destination is not a valid folder.";
-		}
-	}
-	
-	function progress($sourceDir, $destinationDir){
-		$handleS = opendir($sourceDir);
-		while($file = readdir($handleS)){
-			$s[] = $file;
-		}
-		$sourceCount = count($s);
-		closedir($handleS);
-		
-		$handleD = opendir($destinationDir);
-		while($file = readdir($handleD)){
-			$d[] = $file;
-		}
-		$destinationCount = count($d);
-		closedir($handleD);
-		
-		echo "
-			<div id='container' style='padding:4px; margin: 4px; border: solid 1px #ddd;'><div class='inner' style='height: 40px; border: solid 1px #ccc; padding: 4px; margin: 2px; background-color: green; color: white; width: ".round((($destinationCount/$sourceCount)*100))."%'>".round((($destinationCount/$sourceCount)*100))."%"."</div></div>";
-	}
-	
-	function progressBar($sourceDir, $destinationDir){
-		$handleS = opendir($sourceDir);
-		while($file = readdir($handleS)){
-			$s[] = $file;
-		}
-		$sourceCount = count($s);
-		closedir($handleS);
-		
-		$handleD = opendir($destinationDir);
-		while($file = readdir($handleD)){
-			$d[] = $file;
-		}
-		$destinationCount = count($d);
-		closedir($handleD);
-		
-		echo "
-			<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
-			<html xmlns=\"http://www.w3.org/1999/xhtml\">
-			<head>
-			<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
-			<title>PHP Image resizer</title>
-			<script type='application/javascript' src='jQuery.js'></script>
-			<script type='application/javascript'>
-				jQuery(document).ready(function($){
-					setInterval(function(){
-						$('div.container').load('progress.php?source=".(str_replace("/", "%2F", $_GET['source']))."&destination=".(str_replace("/", "%2F", $_GET['destination']))."');
-					}, 100)
-				});
-			</script>
-			</head>
-			<body>
-			
-			<div id='container'><div class='inner'></div></div>
-			</body>
-			</html>
-		";
-	}
 }
